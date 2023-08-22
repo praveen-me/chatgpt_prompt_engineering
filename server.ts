e67b48bac08c50308fb17dc5e1bfcefa1ef534a0
praveen-me <praveenkrsaini0@gmail.com>
2023-08-22 20:46:51 +0530

    Added openai setup and tactics for first priciple

--- server.ts
+++ server.ts
@@ -1,13 +1,17 @@
-import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import express from "express";
+
+import chapterOneCompletions from "./chapters/1";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello from TypeScript server!");
+app.get("/", async (req, res) => {
+  const responses = await chapterOneCompletions();
+
+  res.status(201).send(responses);
 });
 
 app.listen(PORT, () => {
